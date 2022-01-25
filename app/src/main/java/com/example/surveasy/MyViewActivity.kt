4210182d8aa9467c642399cf474e2fe44fba81a6
seoyamin <seoyamin@ewhain.net>
2022-01-26 06:48:25 +0900

    add activity_ to layouts

--- app/src/main/java/com/example/surveasy/MyViewActivity.kt
+++ app/src/main/java/com/example/surveasy/MyViewActivity.kt
@@ -7,6 +7,6 @@ class MyViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_myviewnoticelist)
+        setContentView(R.layout.activity_myview)
     }
 }
