362c0b899dd9be696489c110a9f67a7c5a96b367
young <syoh0313@yonsei.ac.kr>
2022-01-26 11:56:27 +0900

    toolbar

--- app/src/main/java/com/example/surveasy/MyViewNoticeListActivity.kt
+++ app/src/main/java/com/example/surveasy/MyViewNoticeListActivity.kt
@@ -4,17 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class MyViewNoticeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_myviewnoticelist)
-//
-//        val noticeDetailBackBtn : ImageView = findViewById(R.id.MyViewNoticeList_BackBtn)
-//        noticeDetailBackBtn.setOnClickListener{
-//            onBackPressed()
-//        }
+
+        val noticeListToolbar : Toolbar? = findViewById(R.id.ToolbarMyViewNoticeList)
+        setSupportActionBar(noticeListToolbar)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+        }
+
+        noticeListToolbar?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
 
     }
 }
