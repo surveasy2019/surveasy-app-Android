45f34da66ed492680817f2cbae37fc8ee65763c9
young <syoh0313@yonsei.ac.kr>
2022-02-10 15:55:32 +0900

    aa

--- app/src/main/java/com/example/surveasy/list/SurveyInfoViewModel.kt
+++ app/src/main/java/com/example/surveasy/list/SurveyInfoViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.surveasy.list
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SurveyInfoViewModel:ViewModel() {
+
     val surveyInfo = ArrayList<SurveyItems>()
 
     fun setData(surveyItems: SurveyItems){
         surveyInfo.add(surveyItems)
+
+
     }
+
+
 }
