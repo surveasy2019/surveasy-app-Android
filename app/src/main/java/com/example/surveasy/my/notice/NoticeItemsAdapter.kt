63a81e50494e6245aaa091e89750d3ba29d33539
seoyamin <seoyamin@ewhain.net>
2022-02-18 00:09:33 +0900

    MySetting_Withdraw fin

--- app/src/main/java/com/example/surveasy/my/notice/NoticeItemsAdapter.kt
+++ app/src/main/java/com/example/surveasy/my/notice/NoticeItemsAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.surveasy.my.notice
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.surveasy.R
 
@@ -19,6 +21,16 @@ class NoticeItemsAdapter(val noticeList : ArrayList<NoticeItems>)
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val notice : NoticeItems = noticeList[position]
         holder.noticeItemTitle.text = notice.title
+        holder.noticeItemDate.text = notice.date
+        holder.noticeItemContent.text = notice.content
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView!!.context, MyViewNoticeListDetailActivity::class.java)
+            intent.putExtra("title", holder.noticeItemTitle.text)
+            intent.putExtra("date", holder.noticeItemDate.text)
+            intent.putExtra("content", holder.noticeItemContent.text)
+            ContextCompat.startActivity(holder.itemView.context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -27,6 +39,9 @@ class NoticeItemsAdapter(val noticeList : ArrayList<NoticeItems>)
 
     inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val noticeItemTitle : TextView = itemView.findViewById(R.id.NoticeItem_Title)
+        val noticeItemDate: TextView = itemView.findViewById(R.id.NoticeItem_Date)
+        val noticeItemContent : TextView = itemView.findViewById(R.id.NoticeItem_Content)
+
     }
 
 
