77d5180153ba2903b87358328e846308469b867c
young <syoh0313@yonsei.ac.kr>
2022-05-23 13:23:16 +0900

    aa

--- app/src/main/java/com/surveasy/surveasy/list/SurveyItemsAdapter.kt
+++ app/src/main/java/com/surveasy/surveasy/list/SurveyItemsAdapter.kt
@@ -2,6 +2,7 @@ package com.surveasy.surveasy.list
 
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
