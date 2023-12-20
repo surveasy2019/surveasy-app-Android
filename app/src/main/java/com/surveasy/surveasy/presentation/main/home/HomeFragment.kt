e838cf0ccdddd8c7e65492a0601fc032f470fa75
young <syoh0313@yonsei.ac.kr>
2023-12-20 17:14:27 +0900

    feat : bottom bar custom

--- app/src/main/java/com/surveasy/surveasy/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/home/HomeFragment.kt
@@ -1,19 +1,9 @@
 package com.surveasy.surveasy.presentation.main.home
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.navigation.Navigation
 import com.surveasy.surveasy.R
 import com.surveasy.surveasy.databinding.FragmentHomeBinding
 import com.surveasy.surveasy.presentation.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val navController = Navigation.findNavController(view)
-        Log.d("TEST", "home")
-    }
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
 }
--- app/src/main/java/com/surveasy/surveasy/presentation/main/my/MyFragment.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/my/MyFragment.kt
@@ -1,21 +1,9 @@
 package com.surveasy.surveasy.presentation.main.my
 
-import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.surveasy.surveasy.R
 import com.surveasy.surveasy.databinding.FragmentMyBinding
 import com.surveasy.surveasy.presentation.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MyFragment : BaseFragment<FragmentMyBinding>(R.layout.fragment_my) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.d("TEST", "my")
-    }
 
 }
