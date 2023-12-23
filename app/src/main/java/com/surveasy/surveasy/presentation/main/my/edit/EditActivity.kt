28df7dec7a31044e107bcf5182571947273ec8e9
young <syoh0313@yonsei.ac.kr>
2023-12-24 01:49:17 +0900

    feat : my info edit layout

--- app/src/main/java/com/surveasy/surveasy/presentation/main/my/edit/EditActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/my/edit/EditActivity.kt
@@ -1,10 +1,11 @@
 package com.surveasy.surveasy.presentation.main.my.edit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.surveasy.surveasy.R
+import com.surveasy.surveasy.databinding.ActivityEditBinding
+import com.surveasy.surveasy.presentation.base.BaseActivity
 
-class EditActivity : AppCompatActivity() {
+class EditActivity : BaseActivity<ActivityEditBinding>(ActivityEditBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
--- app/src/main/java/com/surveasy/surveasy/presentation/main/my/history/HistoryActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/my/history/HistoryActivity.kt
@@ -1,12 +1,20 @@
 package com.surveasy.surveasy.presentation.main.my.history
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.surveasy.surveasy.R
+import com.surveasy.surveasy.databinding.ActivityHistoryBinding
+import com.surveasy.surveasy.presentation.base.BaseActivity
 
-class HistoryActivity : AppCompatActivity() {
+class HistoryActivity : BaseActivity<ActivityHistoryBinding>(ActivityHistoryBinding::inflate) {
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
 }
