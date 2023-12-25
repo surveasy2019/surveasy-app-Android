e6842da144e3b2fc3d2c7ba0dd77c963deed5968
young <syoh0313@yonsei.ac.kr>
2023-12-25 16:13:45 +0900

    feat : my info edit 수정 흐름 완성

--- app/src/main/java/com/surveasy/surveasy/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.surveasy.surveasy.presentation.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Lifecycle
@@ -30,5 +31,9 @@ abstract class BaseActivity<T : ViewDataBinding>(private val inflater: (LayoutIn
 
     protected inline fun bind(crossinline action: T.() -> Unit) {
         binding.run(action)
+    }
+
+    fun showToastMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/surveasy/surveasy/presentation/main/my/edit/MyEditViewModel.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/my/edit/MyEditViewModel.kt
@@ -6,27 +6,40 @@ import com.surveasy.surveasy.domain.base.BaseState
 import com.surveasy.surveasy.domain.usecase.QueryPanelDetailInfoUseCase
 import com.surveasy.surveasy.presentation.main.my.mapper.toUiPanelDetailData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyEditViewModel @Inject constructor(
     private val queryPanelDetailInfoUseCase: QueryPanelDetailInfoUseCase
 ) : ViewModel() {
+
+    val editPhone = MutableStateFlow("")
+    val editBankIndex = MutableStateFlow(-1)
+    val editBank = MutableStateFlow("")
+    val editAccount = MutableStateFlow("")
+    val editOwner = MutableStateFlow("")
+    val editEnglish = MutableStateFlow(false)
+
     private val _uiState = MutableStateFlow(MyEditUiState())
     val uiState: StateFlow<MyEditUiState> = _uiState.asStateFlow()
 
-//    private val _events = MutableSharedFlow<MyEditUiState>(
-//        replay = 0,
-//        extraBufferCapacity = 1,
-//        onBufferOverflow = BufferOverflow.DROP_OLDEST
-//    )
-//    val events: SharedFlow<MyUiEvents> = _events.asSharedFlow()
+    private val _events = MutableSharedFlow<MyEditUiEvents>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val events: SharedFlow<MyEditUiEvents> = _events.asSharedFlow()
 
     fun queryPanelDetailInfo() {
         queryPanelDetailInfoUseCase().onEach { state ->
@@ -53,9 +66,41 @@ class MyEditViewModel @Inject constructor(
             }
         }.launchIn(viewModelScope)
     }
+
+    fun onClickEditBtn() {
+        if (uiState.value.editMode) {
+            editDone()
+        } else {
+            setEditMode()
+        }
+    }
+
+    private fun setEditMode() {
+        _uiState.update { state ->
+            state.copy(editMode = true)
+        }
+        with(uiState.value) {
+            editPhone.value = phoneNumber
+            editBank.value = accountType
+            editAccount.value = accountNumber
+            editOwner.value = accountOwner
+            editEnglish.value = english
+        }
+    }
+
+    fun observeEnglish(isChecked: Boolean) {
+        editEnglish.value = isChecked
+    }
+
+    private fun editDone() {
+        _uiState.update { state -> state.copy(editMode = false) }
+        viewModelScope.launch { _events.emit(MyEditUiEvents.DoneEdit) }
+
+    }
 }
 
 data class MyEditUiState(
+    val editMode: Boolean = false,
     val name: String = "",
     val birth: String = "",
     val gender: String = "",
@@ -66,3 +111,7 @@ data class MyEditUiState(
     val accountNumber: String = "",
     val english: Boolean = false
 )
+
+sealed class MyEditUiEvents {
+    data object DoneEdit : MyEditUiEvents()
+}
