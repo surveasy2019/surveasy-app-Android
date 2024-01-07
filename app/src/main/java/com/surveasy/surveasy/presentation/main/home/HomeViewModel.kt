619f24e75f44fb7eac82bdec16e43030d348bfb9
young <syoh0313@yonsei.ac.kr>
2024-01-07 17:44:57 +0900

    feat : home survey 나타내기

--- app/src/main/java/com/surveasy/surveasy/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/home/HomeViewModel.kt
@@ -7,7 +7,9 @@ import com.surveasy.surveasy.domain.base.BaseState
 import com.surveasy.surveasy.domain.usecase.GetTempTokenUseCase
 import com.surveasy.surveasy.domain.usecase.ListHomeSurveyUseCase
 import com.surveasy.surveasy.domain.usecase.QueryPanelInfoUseCase
+import com.surveasy.surveasy.presentation.main.home.mapper.toUiHomeListData
 import com.surveasy.surveasy.presentation.main.home.mapper.toUiPanelData
+import com.surveasy.surveasy.presentation.main.home.model.UiHomeListData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -19,6 +21,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,20 +33,29 @@ class HomeViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
-//    private val _events = MutableSharedFlow<HomeEvents>(
-//        replay = 0,
-//        extraBufferCapacity = 1,
-//        onBufferOverflow = BufferOverflow.DROP_OLDEST
-//    )
-//    val events: SharedFlow<HomeEvents> = _events.asSharedFlow()
+    private val _events = MutableSharedFlow<HomeEvents>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val events: SharedFlow<HomeEvents> = _events.asSharedFlow()
 
-    init {
-        listHomeSurvey()
-    }
 
     fun listHomeSurvey() {
-        listHomeSurveyUseCase().onEach {
-            Log.d("TEST", "$it")
+        listHomeSurveyUseCase().onEach { state ->
+            when (state) {
+                is BaseState.Success -> {
+                    state.data.let { survey ->
+                        _uiState.update {
+                            val data =
+                                survey.surveyAppHomeList.map { list -> list.toUiHomeListData() }
+                            it.copy(list = data)
+                        }
+                    }
+                }
+
+                else -> Log.d("TEST", "failed")
+            }
         }.launchIn(viewModelScope)
     }
 
@@ -77,6 +89,14 @@ class HomeViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
+    fun navigateToSurveyDetail(id: Int) {
+        viewModelScope.launch { _events.emit(HomeEvents.ClickSurveyItem(id)) }
+    }
+
+}
+
+sealed class HomeEvents {
+    data class ClickSurveyItem(val id: Int) : HomeEvents()
 }
 
 data class HomeUiState(
@@ -84,4 +104,5 @@ data class HomeUiState(
     val count: Int = 0,
     val rewardCurrent: Int = 0,
     val rewardTotal: Int = 0,
+    val list: List<UiHomeListData> = emptyList(),
 )
