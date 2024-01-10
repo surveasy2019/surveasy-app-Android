69ccd89ff89bd99c31819d73f888de7acff0b084
young <syoh0313@yonsei.ac.kr>
2024-01-10 20:24:02 +0900

    feat : splash -> tutorial page 이동 구현

--- app/src/main/java/com/surveasy/surveasy/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/splash/SplashActivity.kt
@@ -1,85 +1,59 @@
 package com.surveasy.surveasy.presentation.splash
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.surveasy.surveasy.R
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.activity.viewModels
+import com.surveasy.surveasy.databinding.ActivitySplashBinding
+import com.surveasy.surveasy.presentation.base.BaseActivity
+import com.surveasy.surveasy.presentation.intro.IntroActivity
+import com.surveasy.surveasy.presentation.introduce.FirstIntroduceActivity
+import com.surveasy.surveasy.presentation.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
-class SplashActivity : AppCompatActivity() {
-    var token = ""
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+    private val viewModel: SplashViewModel by viewModels()
 
-
-//        // Initialization of Amplitude
-//        val client = Amplitude.getInstance()
-//            .initialize(getApplicationContext(), "ae22fbd62558adb236f993284cc62c71")
-//            .enableForegroundTracking(application)
-//
-//
-//        if(isConnectInternet() != "null"){
-//            val handler = Handler(Looper.getMainLooper())
-//            handler.postDelayed({
-//
-//                finish()
-//
-//            },4000)
-//        }else{
-//            val handler = Handler(Looper.getMainLooper())
-////            handler.postDelayed({
-////                val intent = Intent(baseContext, NetworkAlertActivity::class.java)
-////                startActivity(intent)
-////                finish()
-//
-//            //},4000)
-//        }
-//
-//
-//
-//        supportActionBar?.hide()
-//
-//        FirebaseMessaging.getInstance().token.addOnCompleteListener(
-//            OnCompleteListener { task ->
-//                if (!task.isSuccessful) {
-//                    Log.w(
-//                        ContentValues.TAG,
-//                        "Fetching FCM registration token failed",
-//                        task.exception
-//                    )
-//                    return@OnCompleteListener
-//                }
-//                token = task.result
-//
-//            })
-//
-//
-//        db.collection("AndroidFirstScreen").get()
-//            .addOnSuccessListener { result ->
-//                nextActivity()
-////                var i = 0
-////                for(document in result){   if(document.id == token) i++   }
-////                if(i==0){
-////                    startActivity(Intent(this,FirstIntroduceScreenActivity::class.java))
-////                    finish()
-////                }
-////                else {
-////                    nextActivity()
-////
-//////                    var bannerIndex = 0
-//////                    for(item in bannerModel.uriList) {
-//////                        intent.putExtra(item, "banner" + bannerIndex.toString())
-//////                        bannerIndex++
-//////                    }
-////                }
-//            }
-
+    override fun initView() {
+        binding.vm = viewModel
     }
 
+    override fun initData() = Unit
 
-//
+    override fun initEventObserver() {
+        repeatOnStarted {
+            delay(2000)
+            viewModel.getAutoLogin()
+        }
+        repeatOnStarted {
+            viewModel.events.collect {
+                when (it) {
+                    is SplashUiEvent.NavigateToTutorial -> navigateToTutorial()
+                    is SplashUiEvent.NavigateToIntro -> navigateToMain()
+                    is SplashUiEvent.NavigateToMain -> navigateToIntro()
+                }
+            }
+        }
+    }
 
+    private fun navigateToTutorial() {
+        val intent = Intent(this, FirstIntroduceActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
-
+    private fun navigateToIntro() {
+        val intent = Intent(this, IntroActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
