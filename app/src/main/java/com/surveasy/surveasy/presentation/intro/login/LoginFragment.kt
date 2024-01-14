54cbf567b77190105a9502675cd1d527acf62d43
young <syoh0313@yonsei.ac.kr>
2024-01-14 16:21:34 +0900

    chore : 사용자 정보 요청 로그

--- app/src/main/java/com/surveasy/surveasy/presentation/intro/login/LoginFragment.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/intro/login/LoginFragment.kt
@@ -60,6 +60,18 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
         if (error != null) {
             Log.d("TEST", "fail, $error")
         } else if (token != null) {
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.d("TEST", "사용자 정보 요청 실패", error)
+                }
+                else if (user != null) {
+                    Log.d("TEST", "사용자 정보 요청 성공" +
+                            "\n회원번호: ${user.id}" +
+                            "\n이메일: ${user.kakaoAccount?.email}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                }
+            }
             Log.d("TEST", "success, $token")
         }
     }
