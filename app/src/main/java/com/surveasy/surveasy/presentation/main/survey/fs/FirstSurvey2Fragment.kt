33f181e666a8577b5ed6cf898fdb01b7db2f404a
young <syoh0313@yonsei.ac.kr>
2024-01-14 17:51:51 +0900

    feature : first survey layout 구현

---
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/survey/fs/FirstSurvey2Fragment.kt
@@ -0,0 +1,21 @@
+package com.surveasy.surveasy.presentation.main.survey.fs
+
+import com.surveasy.surveasy.R
+import com.surveasy.surveasy.databinding.FragmentFirstSurvey2Binding
+import com.surveasy.surveasy.presentation.base.BaseFragment
+
+
+class FirstSurvey2Fragment :
+    BaseFragment<FragmentFirstSurvey2Binding>(R.layout.fragment_first_survey2) {
+    override fun initData() {
+
+    }
+
+    override fun initEventObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
---
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/survey/fs/FirstSurveyActivity.kt
@@ -0,0 +1,19 @@
+package com.surveasy.surveasy.presentation.main.survey.fs
+
+import com.surveasy.surveasy.databinding.ActivityFirstSurveyBinding
+import com.surveasy.surveasy.presentation.base.BaseActivity
+
+class FirstSurveyActivity :
+    BaseActivity<ActivityFirstSurveyBinding>(ActivityFirstSurveyBinding::inflate) {
+    override fun initData() {
+
+    }
+
+    override fun initEventObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
