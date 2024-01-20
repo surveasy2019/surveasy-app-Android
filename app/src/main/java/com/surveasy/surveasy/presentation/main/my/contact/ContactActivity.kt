3bbeae90d6c901e7f9ebb35bf74ad3554d6f4aa3
young <syoh0313@yonsei.ac.kr>
2024-01-20 20:19:59 +0900

    feat : mypage contact 구현

--- app/src/main/java/com/surveasy/surveasy/presentation/main/my/contact/ContactActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/my/contact/ContactActivity.kt
@@ -1,12 +1,27 @@
 package com.surveasy.surveasy.presentation.main.my.contact
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.ClipData
+import android.content.ClipboardManager
 import com.surveasy.surveasy.R
+import com.surveasy.surveasy.databinding.ActivityContactBinding
+import com.surveasy.surveasy.presentation.base.BaseActivity
 
-class ContactActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact)
+class ContactActivity : BaseActivity<ActivityContactBinding>(ActivityContactBinding::inflate) {
+    override fun initData() {
+
+    }
+
+    override fun initEventObserver() {
+
+    }
+
+    override fun initView() = with(binding) {
+        btnCopy.setOnClickListener {
+            val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("text", resources.getText(R.string.my_email))
+            clipboardManager.setPrimaryClip(clipData)
+
+            showToastMessage("이메일 주소가 복사되었습니다.")
+        }
     }
 }
