7f2b226a655309b48474facdc4829ce62f7ebb94
young <syoh0313@yonsei.ac.kr>
2024-01-25 00:32:37 +0900

    feat : fs api 연결

--- app/src/main/java/com/surveasy/surveasy/domain/usecase/CreateFsResponseUseCase.kt
+++ app/src/main/java/com/surveasy/surveasy/domain/usecase/CreateFsResponseUseCase.kt
@@ -22,17 +22,73 @@ class CreateFsResponseUseCase @Inject constructor(
         pet: Int,
     ): Flow<BaseState<Unit>> = repository.createFsResponse(
         english,
-        city,
+        setCity(city),
         district,
         setFamily(family),
         setHouseType(houseType),
-        job,
+        setJob(job),
         university,
-        major,
+        setMajor(major),
         setMarriage(marriage),
         setMilitary(military),
         setPet(pet)
     )
+
+    private fun setCity(city: String): String {
+        return when (city) {
+            "서울특별시" -> "SEOUL"
+            "부산광역시" -> "BUSAN"
+            "대구광역시" -> "DAEGU"
+            "인천광역시" -> "INCHEON"
+            "광주광역시" -> "GWANGJU"
+            "대전광역시" -> "DAEJEON"
+            "울산광역시" -> "ULSAN"
+            "세종특별자치시" -> "SEJONG"
+            "경기도" -> "GYEONGGI"
+            "강원도" -> "GANGWON"
+            "충청북도" -> "CHUNGBUK"
+            "충청남도" -> "CHUNGNAM"
+            "전라북도" -> "JEONBUK"
+            "전라남도" -> "JEONNAM"
+            "경상북도" -> "GYEONGBUK"
+            "경상남도" -> "GYEONGNAM"
+            "제주특별자치도" -> "JEJU"
+            else -> ""
+        }
+    }
+
+    private fun setJob(job: String): String {
+        return when (job) {
+            "중/고등학생" -> "MID_HIGH"
+            "대학생" -> "UNDERGRADUATE"
+            "대학원생" -> "GRADUATE"
+            "사무직" -> "OFFICE"
+            "경영 관리직" -> "MANAGEMENT"
+            "판매/서비스직" -> "SALES"
+            "자영업" -> "BUSINESS"
+            "기능/생산직" -> "PRODUCTION"
+            "전문직" -> "SPECIALIZED"
+            "농림어업" -> "FARMING_FISHING"
+            "전업주부" -> "HOMEMAKER"
+            "무직" -> "NONE"
+            "기타" -> "ETC"
+            else -> ""
+        }
+    }
+
+    private fun setMajor(major: String): String {
+        return when (major) {
+            "사회계열" -> "SOCIAL"
+            "공학계열" -> "ENGINEERING"
+            "인문계열" -> "HUMAN"
+            "자연계열" -> "NATURE"
+            "예체능계열" -> "ART_PHYSICAL"
+            "의약계열" -> "MEDICAL"
+            "교육계열" -> "EDUCATION"
+            "기타" -> "ETC"
+            else -> ""
+        }
+    }
 
     private fun setFamily(family: String): String {
         return when (family) {
@@ -76,7 +132,7 @@ class CreateFsResponseUseCase @Inject constructor(
 
     private fun setPet(pet: Int): String {
         return when (pet) {
-            0 -> "DONE"
+            0 -> "NONE"
             1 -> "CAT"
             2 -> "DOG"
             3 -> "ETC"
--- app/src/main/java/com/surveasy/surveasy/presentation/main/survey/fs/FsActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/survey/fs/FsActivity.kt
@@ -1,5 +1,6 @@
 package com.surveasy.surveasy.presentation.main.survey.fs
 
+import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.surveasy.surveasy.R
@@ -9,11 +10,21 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FsActivity : BaseActivity<ActivityFsBinding>(ActivityFsBinding::inflate) {
+    private val viewModel: FsViewModel by viewModels()
     private var navController: NavController? = null
 
     override fun initData() = Unit
 
-    override fun initEventObserver() = Unit
+    override fun initEventObserver() {
+        repeatOnStarted {
+            viewModel.events.collect {
+                when (it) {
+                    is FsEvents.NavigateToList -> finish()
+                    else -> Unit
+                }
+            }
+        }
+    }
 
     override fun initView() {
         val navHostFragment =
