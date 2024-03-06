279935cdbb40197bd494af97e29d1756c5c7206f
young <syoh0313@yonsei.ac.kr>
2024-03-06 22:53:52 +0900

    update : version 2.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.surveasy.surveasy"
         minSdk = 24
         targetSdk = 34
-        versionCode = 44
-        versionName = "2.0.4"
+        versionCode = 45
+        versionName = "2.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
