9e527cd66b9ea9a168ef565bc6fcb6cac3f4e450
young <syoh0313@yonsei.ac.kr>
2024-03-20 01:21:53 +0900

    feat : remote config 세팅 및 업데이트 알림 구현

--- app/src/main/java/com/surveasy/surveasy/presentation/main/MainActivity.kt
+++ app/src/main/java/com/surveasy/surveasy/presentation/main/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.surveasy.surveasy.presentation.main
 
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.surveasy.surveasy.R
 import com.surveasy.surveasy.databinding.ActivityMainBinding
 import com.surveasy.surveasy.presentation.base.BaseActivity
+import com.surveasy.surveasy.presentation.util.showTwoButtonDialog
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
@@ -22,6 +28,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             viewModel.events.collect {
                 when (it) {
                     is MainEvents.FinishApp -> finish()
+                    is MainEvents.UpdateDialog -> {
+                        showTwoButtonDialog(
+                            this@MainActivity,
+                            "업데이트 안내",
+                            resources.getString(R.string.update_warn),
+                            "업데이트 하러 가기",
+                            "",
+                            { goStore() }
+                        ) {}
+                    }
                 }
             }
         }
@@ -32,6 +48,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         binding.bnvNavBar.setupWithNavController(navController)
+        val appVersion = packageManager.getPackageInfo(packageName, 0).versionName
+        Log.d("TAG", "initView: $appVersion")
+        lifecycleScope.launch { viewModel.checkVersion(appVersion) }
+
+    }
+
+    private fun goStore() {
+        val uri = "https://play.google.com/store/apps/details?id=com.surveasy.surveasy"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        startActivity(intent)
     }
 
 }
